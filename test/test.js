095b43103372665e787b0d187c0a09a25b744638
m_goet09 <m_goet09@uni-muenster.de>
2016-11-25 09:13:51 +0100

    update test.js

--- test/test.js
+++ test/test.js
@@ -1,12 +1,6 @@
-//test file with mocha,
+//test file with mocha
 
-//Require the dev-dependencies
-let chai = require('chai');
-let chaiHttp = require('chai-http');
-let should = chai.should();
-
-chai.use(chaiHttp);
-
+//test the png
 describe('Test the get.app method with png', function() {
   describe('GET http://localhost:3000/api/1.0/badge/doaj/1234?type=png', function() {
     it('returns an image', function() {
@@ -15,6 +9,7 @@ describe('Test the get.app method with png', function() {
   });
 });
 
+//test the svg
 describe('Test the get.app method with svg', function() {
   describe('GET http://localhost:3000/api/1.0/badge/doaj/1234?type=svg', function() {
     it('returns an image', function() {
