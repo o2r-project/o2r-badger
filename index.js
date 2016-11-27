04aa8cc29308d5572dedc907dc39ac4d8f7b6ed3
c_rend02 <c_rend02@uni-muenster.de>
2016-11-27 17:51:18 +0100

    solved existing file error

--- index.js
+++ index.js
@@ -10,21 +10,21 @@ app.get(base + '/doaj/:doi', function(req, res) {
 	var picwidth = req.query.width;
 	//var picheight= req.query.height // OPTIONAL
 	var type = req.query.type;
-	var badgename = "christmas"
+	var badgename = "whale"
 	
 	if (type == "png"){
 		// convert image from svg to png
 		if(picwidth != null){
-			//todo: add max and min width and height conditions
+
 			console.log("parse badge from svg to png with width "+ picwidth);
 
 			filename=__dirname+'/'+badgename+".svg"
 			const input = fs.readFileSync(filename)
 			const output = svg2png.sync(input, { width: picwidth, filename: filename}); //OPTIONAL HEIGHT ARGUMENT: (input, { width: picwidth, height: picheight, filename: filename}) 
-			const outputFilename = badgename+".png";
+			const outputFilename = badgename+picwidth+".png";
 			fs.writeFileSync(outputFilename, output, { flag: "wx" });
 			console.log("return resized png");
-			res.sendFile(__dirname+'/'+badgename+".png");
+			res.sendFile(__dirname+'/'+badgename+picwidth+".png");
 		}	
 		else{
 		// convert svg to png
