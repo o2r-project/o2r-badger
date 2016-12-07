10b5574232a97f80608c9e6b51bc48f124490e70
nuest <daniel.nuest@uni-muenster.de>
2016-12-07 15:22:06 +0100

    fix tests

--- test/test.js
+++ test/test.js
@@ -2,20 +2,50 @@
 var server = require('../index');
 var chai = require('chai');
 var chaiHttp = require('chai-http');
+var sizeOf = require('image-size');
+var fs = require('fs');
+var StringDecoder = require('string_decoder').StringDecoder;
+var decoder = new StringDecoder('utf8');
 var should = chai.should();
 var expect = chai.expect;
 chai.use(chaiHttp);
 
-
 //test the png
 describe('Test the get.app method with png and width', function () {
   describe('GET localhost:3000/api/1.0/badge/executable/o2r/id?format=png&width=100', function () {
-    it('should return an image', function () {
+    it('should have OK response status', function (done) {
       chai.request(server)
         .get('/api/1.0/badge/executable/o2r/id?format=png&width=100')
         .end(function (err, res) {
           res.should.have.status(200);
+          done();
+        });
+    });
+    it('should return some content', function (done) {
+      chai.request(server)
+        .get('/api/1.0/badge/executable/o2r/id?format=png')
+        .end(function (err, res) {
           res.body.should.not.be.empty;
+          done();
+        });
+    });
+    it('should return the correct image type', function (done) {
+      chai.request(server)
+        .get('/api/1.0/badge/executable/o2r/id?format=png')
+        .end(function (err, res) {
+          var dimensions = sizeOf(res.body);
+          expect(dimensions.type).to.eql('png');
+          res.type.should.eql('image/png');
+          res.headers['content-type'].should.eql('image/png');
+          done();
+        });
+    });
+    it('should return the correct image size', function (done) {
+      chai.request(server)
+        .get('/api/1.0/badge/executable/o2r/id?format=png&width=42')
+        .end(function (err, res) {
+          var dimensions = sizeOf(res.body);
+          expect(dimensions.width).to.eql(42);
           done();
         });
     });
@@ -25,31 +55,18 @@ describe('Test the get.app method with png and width', function () {
 //test the svg
 describe('Test the get.app method with svg', function () {
   describe('GET localhost:3000/api/1.0/badge/licence/o2r/id?format=svg', function () {
-    it('should return an image', function () {
+    it('should return svg', function (done) {
       chai.request(server)
         .get('/api/1.0/badge/licence/o2r/id?format=svg')
         .end(function (err, res) {
           res.should.have.status(200);
-          res.body.should.not.be.empty;
+          res.should.not.be.empty;
+          var svgstring = decoder.end(res.body);
+          //console.log(svgstring);
+          svgstring.should.include('<svg xmlns="http://www.w3.org/2000/svg"');
           done();
         });
     });
   });
 });
 
-
-
-//test the png with width
-describe('Test the get.app method with png', function () {
-  describe('GET localhost:3000/api/1.0/badge/peerreviewed/o2r/id?format=png', function () {
-    it('should return an image', function () {
-      chai.request(server)
-        .get('/api/1.0/badge/peerreviewed/o2r/id?format=png')
-        .end(function (err, res) {
-          res.should.have.status(200);
-          res.body.should.not.be.empty;
-          done();
-        });
-    });
-  });
-});
