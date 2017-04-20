a77f8cfbcfed797ff7f17c45e902038e6572b662
LukasLohoff <l_loho01@uni-muenster.de>
2017-04-20 14:22:43 +0200

    integrate geocontainer-badges/release

---
+++ controllers/release-date/release-date.js
@@ -0,0 +1,174 @@
+var crossref = "http://api.crossref.org/works/";
+
+/**
+ * Include services used for the application
+ */
+var request = require('request');
+
+exports.getReleaseDateBadge = (req, res) => {
+
+    var doi = req.params.doi;
+    doi = doi.split(":");
+    doi = doi[1];
+    var extended = req.params.extended;
+    var badge;
+
+    // Request to crossref to get information for the paper with the given doi
+    request(
+        {
+        url: crossref + doi,
+        proxy: "http://wwwproxy.uni-muenster.de:80"
+        },
+        function (error, response, body) {
+
+        // error handling of request to crossref
+        if (error) {
+            console.log(error);
+        }
+        if (!response || response.status == 404 || response.statusCode == 404) {
+            res.status(404).send('not available');
+            return;
+        } else if (response.status == 500 || response.statusCode == 500 || error) {
+            res.status(500).send('Unable to find data on server: %s', error);
+            return;
+        }
+
+        // get the created date (if available)
+        if(response.body != undefined){
+            // parse the response to json
+            var jsonResponse = JSON.parse(response.body);
+            if(jsonResponse.message.issued != undefined){
+                // get the issued parameter
+                var issued = jsonResponse.message.issued;
+                if(issued["date-parts"] != undefined || issued["date-parts"] != ""){
+                    // get the date part (containing the release date)
+                    var date = issued["date-parts"][0];
+                    console.log(date);
+                } else {
+                    sendUndefined();
+                    return;
+                }
+            } else {
+                sendUndefined();
+                return;
+            }
+        } else {
+            sendUndefined();
+            return;
+        }
+
+        // send the no information badge
+        function sendUndefined() {
+            if(extended == 'extended'){
+                badge = 'badges/released_no_information.svg';
+                res.sendFile(badge, { root : __dirname}, function (err) {
+                    if (err) {
+                        console.log(err);
+                        res.status(err.status).end();
+                    }
+                    else console.log('Sent file: ', badge);
+                });
+            } else {
+                res.redirect('https://img.shields.io/badge/release%20time-n%2Fa-lightgrey.svg');
+            }
+        }
+
+        /*************** send big badges *************/
+        // todo take leapyears into account
+        if (extended == 'extended') {
+
+            // check if day and month are given
+            // if not, set them to 0
+            var releaseDay = 0;
+            if(date[2] != undefined)
+                releaseDay = date[2];
+            var releaseMonth = 0;
+            if(date[1] != undefined)
+                releaseMonth = date[1];
+
+            var currentDate = new Date().getTime();
+            var releaseDate = new Date(date[0], releaseMonth, releaseDay, 0, 0, 0, 0).getTime();
+
+
+            if (releaseDate > currentDate-31536000000) {
+                badge = 'badges/released_year.svg';
+                res.sendFile(badge, { root : __dirname}, function (err) {
+                    if (err) {
+                        console.log(err);
+                        res.status(err.status).end();
+                    }
+                    else console.log('Sent file: ', badge);
+                });
+            }
+            else if (releaseDate > currentDate-157680000000) {
+                badge = 'badges/released_5_year.svg';
+                res.sendFile(badge, { root : __dirname}, function (err) {
+                    if (err) {
+                        console.log(err);
+                        res.status(err.status).end();
+                    }
+                    else console.log('Sent file: ', badge);
+                });
+            }
+            else if (releaseDate > currentDate-315360000000) {
+                badge = 'badges/released_10_years.svg';
+                res.sendFile(badge, { root : __dirname}, function (err) {
+                    if (err) {
+                        console.log(err);
+                        res.status(err.status).end();
+                    }
+                    else console.log('Sent file: ', badge);
+                });
+            }
+            else if (releaseDate > currentDate-630720000000) {
+                badge = 'badges/released_20_years.svg';
+                res.sendFile(badge, { root : __dirname}, function (err) {
+                    if (err) {
+                        console.log(err);
+                        res.status(err.status).end();
+                    }
+                    else console.log('Sent file: ', badge);
+                });
+            }
+            else if (releaseDate > currentDate-946080000000) {
+                badge = 'badges/released_30_years.svg';
+                res.sendFile(badge, { root : __dirname}, function (err) {
+                    if (err) {
+                        console.log(err);
+                        res.status(err.status).end();
+                    }
+                    else console.log('Sent file: ', badge);
+                });
+            }
+            else if (releaseDate > currentDate-1261440000000) {
+                badge = 'badges/released_40_years.svg';
+                res.sendFile(badge, { root : __dirname}, function (err) {
+                    if (err) {
+                        console.log(err);
+                        res.status(err.status).end();
+                    }
+                    else console.log('Sent file: ', badge);
+                });
+            }
+            else if (releaseDate < currentDate-1261440000000) {
+                // todo insert new badge
+                badge = 'badges/released_over_50_years.svg';
+                res.sendFile(badge, { root : __dirname}, function (err) {
+                    if (err) {
+                        console.log(err);
+                        res.status(err.status).end();
+                    }
+                    else console.log('Sent file: ', badge);
+                });
+            }
+        }
+
+        /************* send small badges ********************/
+        else {
+            var releaseYear = date[0];
+            // send a badge showing the created date
+            res.redirect('https://img.shields.io/badge/release%20time-' + releaseYear + '-blue.svg');
+        }
+
+    });
+}
--- index.js
+++ index.js
@@ -9,13 +9,15 @@ var decoder = new StringDecoder('utf8');
 
 // load controllers
 var controllers = {};
-controllers.execute = require('./controllers/execute');
-controllers.license = require('./controllers/license');
-controllers.review = require('./controllers/review');
-controllers.release = require('./controllers/release');
-controllers.scale = require('./controllers/scale');
-controllers.serve = require('./controllers/serve');
-controllers.locate = require('./controllers/locate');
+//Badge services
+controllers.executability = require('./controllers/executability/executability');
+controllers.license = require('./controllers/license/license');
+controllers.review = require('./controllers/review-status/review-status');
+controllers.release = require('./controllers/release-date/release-date');
+controllers.location = require('./controllers/location/location');
+//Misc services
+controllers.scaling = require('./controllers/scaling/scaling');
+controllers.server = require('./controllers/server/server');
 
 var server = process.env.SERVER_IP || "http://giv-project6.uni-muenster.de:";//-e
 console.log(server);
@@ -26,22 +28,23 @@ var base = '/api/1.0/badge';
 */
 
 //Scalability:
-app.get('/api/1.0/badge', controllers.scale.getBase);
-app.get('/api/1.0/badge/:type', controllers.scale.getType);
-app.get('/api/1.0/badge/:type/:service', controllers.scale.getService);
-app.get('/api/1.0/badge/:type/:service/:id', controllers.scale.getBadge);
+app.get('/api/1.0/badge', controllers.scaling.getBase);
+app.get('/api/1.0/badge/:type', controllers.scaling.getType);
+app.get('/api/1.0/badge/:type/:service', controllers.scaling.getService);
+app.get('/api/1.0/badge/:type/:service/:id', controllers.scaling.getBadge);
 
 //Executing: 3001
-app.get('/api/1.0/badge/:executable/:o2r/:id/:extended?', controllers.execute.getExecutabilityBadge);
+app.get('/api/1.0/badge/:executable/:o2r/:id/:extended?', controllers.executability.getExecutabilityBadge);
 
 //Licensing 3003
 app.get('/api/1.0/badge/licence/o2r/:id/:extended?', controllers.license.getLicenseBadge);
 
 //Locating: 3005
-app.get('/api/1.0/badge/spatial/o2r/:id', controllers.locate.getSmallSpatialBadge);
-app.get('/api/1.0/badge/spatial/o2r/:id/extended', controllers.locate.getBigSpatialBadge);
+app.get('/api/1.0/badge/spatial/o2r/:id', controllers.location.getSmallSpatialBadge);
+app.get('/api/1.0/badge/spatial/o2r/:id/extended', controllers.location.getBigSpatialBadge);
 
-//Release date:
+//Release date: 3004
+app.get('/api/1.0/badge/:releasetime/:crossref/:doi/:extended?', controllers.release.getReleaseDateBadge);
 
 //Peer review:
 
